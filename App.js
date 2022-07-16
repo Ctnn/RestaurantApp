0f085570ca94862ffe8f55ce41a9d4c1472b5fe1
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-07-16 13:35:18 +0300

    Base ready

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import SearchScreen from './src/screens/SearchScreen';
+
+
+const navigator = createStackNavigator(
+  {
+Search: SearchScreen,
+},
+{
+  initialRouteName:'Search',
+  defaultNavigationOptions:{ //Üst kısıımda yazacak isim
+    title:'BusinessSearch'
+  }
 }
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(navigator);
+
+
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View,Text,StyleSheet} from 'react-native';
+
+function SearchScreen(){
+    return <View>
+<Text>
+    Search Screen
+</Text>
+
+    </View>;
+}
+
+export default SearchScreen;
