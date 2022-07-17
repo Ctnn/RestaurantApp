8360fe9cfedc65924903782f81c9bc2afccead21
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-07-17 11:29:27 +0300

    Putting it all together

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,39 +3,43 @@ import {View,TextInput,StyleSheet} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 //İstediğini curly'ye yaz
 
-function SearchBar(){
-    return <View style={styles.backgroundStyle}>
-<Feather name="search" style={styles.iconStyle}/>
-<TextInput 
-style={styles.TextInput}
-placeholder="Search"
-/>
+const SearchBar = ({ term, onTermChange }) => {
+    return (
+      <View style={styles.backgroundStyle}>
+        <Feather name="search" style={styles.iconStyle} />
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.inputStyle}
+          placeholder="Search"
+          value={term}
+          onChangeText={onTermChange}
+          onEndEditing={()=>onTermSubmit}
+        />
+      </View>
+    );
+  };
+  
 
-    </View>;
-}
-
-const styles=StyleSheet.create({
-backgroundStyle:{
-    marginTop:15,
-backgroundColor:'#F0EEEE',
-height:50,
-borderRadius:5,
-marginHorizontal:15,
-flexDirection:'row',
-
-},
-inputStyle:{
-    borderColor:'black',
-    borderWidth:1,
-    flex:1,
-    fontsize:18
-},
-iconStyle:{
-    fontSize:35,
-    alignSelf:'center',
-    marginHorizontal:15
-}
-
-})
+const styles = StyleSheet.create({
+    backgroundStyle: {
+      marginTop: 10,
+      backgroundColor: '#F0EEEE',
+      height: 50,
+      borderRadius: 5,
+      marginHorizontal: 15,
+      flexDirection: 'row'
+    },
+    inputStyle: {
+      flex: 1,
+      fontSize: 18
+    },
+    iconStyle: {
+      fontSize: 35,
+      alignSelf: 'center',
+      marginHorizontal: 15
+    }
+  });
+  
 
 export default SearchBar;
