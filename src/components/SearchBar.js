4a9095eff73003c7ebf0b2a76f74856b5ffa6c7c
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-07-18 10:25:22 +0300

    Error Handling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,43 +3,41 @@ import {View,TextInput,StyleSheet} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 //İstediğini curly'ye yaz
 
-const SearchBar = ({ term, onTermChange }) => {
-    return (
-      <View style={styles.backgroundStyle}>
-        <Feather name="search" style={styles.iconStyle} />
-        <TextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          style={styles.inputStyle}
-          placeholder="Search"
-          value={term}
-          onChangeText={onTermChange}
-          onEndEditing={()=>onTermSubmit}
-        />
-      </View>
-    );
-  };
-  
-//trysomething
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-    backgroundStyle: {
-      marginTop: 10,
-      backgroundColor: '#F0EEEE',
-      height: 50,
-      borderRadius: 5,
-      marginHorizontal: 15,
-      flexDirection: 'row'
-    },
-    inputStyle: {
-      flex: 1,
-      fontSize: 18
-    },
-    iconStyle: {
-      fontSize: 35,
-      alignSelf: 'center',
-      marginHorizontal: 15
-    }
-  });
-  
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+  }
+});
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
-import {View,Text,StyleSheet} from 'react-native';
+import {View,Text,StyleSheet, Button} from 'react-native';
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
-const SearchScreen = ()=>{
-    const [term, setTerm] = useState('');
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+    //buradaki [] nedeni api içerisindeki 'bussiness:[] içerisindeki
+    //verileri çekerek onları kullanmak adına yapılan bir useState
 
- 
+  const searchApi = async () => {
+    //async or yelp.get('/seach').then but first option make sense
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'ankara'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
 
-    return (
-        <View>
-          <SearchBar 
-          term={term} 
-          onTermChange={newTerm => setTerm(newTerm)}
-          onTermSubmit={()=>console.log('asd')}
-          />
-          <Text>Search Screen</Text>
-          <Text>{term}</Text>
-        </View>
-      );  
+  return (
+    <View>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+    </View>
+  );
 };
+
 const styles = StyleSheet.create({});
 
 export default SearchScreen;
