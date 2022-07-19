46666ac758a488f1da71c29563dd079bac3c01ac
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-07-19 09:25:02 +0300

    Error fix

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -3,4 +3,36 @@ import yelp from "../api/yelp";
 
 export default ()=>{
 
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+      //buradaki [] nedeni api içerisindeki 'bussiness:[] içerisindeki
+      //verileri çekerek onları kullanmak adına yapılan bir useState
+  
+    const searchApi = async searchTerm => {
+      //async or yelp.get('/seach').then but first option make sense
+  
+      try {
+        const response = await yelp.get('/search', {
+          params: {
+            limit: 50,
+            term: searchTerm,
+            location: 'san jose'
+          }
+        });
+        setResults(response.data.businesses);
+      } catch (err) {
+        console.log("this is  the error=>  "+err);
+        setErrorMessage('Something went wrong');
+      }
+    };
+  
+    
+    //Call searchApi when component
+    //is first rendered. BAD CODE !
+    // searchapi('pasta')
+    //It would be run a many mant times
+    useEffect(() => {
+      searchApi('pasta');
+    }, []);
+return [searchApi,results,errorMessage];  
 };
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,42 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {View,Text,StyleSheet, Button} from 'react-native';
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp"
-
+import useResults from "../hooks/useResults";
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-    //buradaki [] nedeni api içerisindeki 'bussiness:[] içerisindeki
-    //verileri çekerek onları kullanmak adına yapılan bir useState
-
-  const searchApi = async searchTerm => {
-    //async or yelp.get('/seach').then but first option make sense
-
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'ankara'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      console.log("this is  the error=>  "+err);
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  
-  //Call searchApi when component
-  //is first rendered. BAD CODE !
-  // searchapi('pasta')
-  //It would be run a many mant times
-  useEffect(() => {
-    searchApi('makarna');
-  }, []);
-
+  const[searchApi,results,errorMessage]=useResults();
   return (
     <View>
       <SearchBar
